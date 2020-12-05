ddeb9dcbc22350a66207884b74f1531239ea0aa4
Nicholas Miklaucic <nicholas.miklaucic@gmail.com>
2020-12-04 20:08:14 -0500

    Improved code by working around "cid" better, thanks Daniel

--- day4/main.hs
+++ day4/main.hs
@@ -1,9 +1,9 @@
 module Main where
 
-import Debug.Trace
 import Data.List.Split
 
-allFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+allFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"]
+reqFields = drop 1 $ reverse allFields
 
 strInRange :: String -> Int -> Int -> Bool
 strInRange str lo hi = (read str >= lo) && (read str <= hi)
@@ -12,7 +12,7 @@ digits = "1234567890"
 hex = "abcdef"
 
 allValidators :: [String -> Bool]
-allValidators = [byr, iyr, eyr, hgt, hcl, ecl, pid] where
+allValidators = [byr, iyr, eyr, hgt, hcl, ecl, pid, cid] where
   byr str = (length str == 4) && strInRange str 1920 2002
   iyr str = (length str == 4) && strInRange str 2010 2020
   eyr str = (length str == 4) && strInRange str 2020 2030
@@ -25,22 +25,23 @@ allValidators = [byr, iyr, eyr, hgt, hcl, ecl, pid] where
   hcl _ = False
   ecl = flip elem ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
   pid str = (length str == 9) && all (`elem` digits) str
+  cid = const True
 
 validField :: String -> String -> Bool
 validField field val = any (f field val) $ zip allFields allValidators where
-  f field val (fieldf, valf) = (field == "cid") || ((field == fieldf) && valf val)
+  f field val (fieldf, valf) = (field == fieldf) && valf val
 
 toPassports :: String -> [String]
 toPassports str = map unwords $ splitOn [""] $ lines str
 
 validPassport1 :: String -> Bool
-validPassport1 str = all (`elem` fields) allFields where
+validPassport1 str = all (`elem` fields) reqFields where
   entries = splitOn " " str
   splitEntries = map (splitOn ":") entries
   fields = map head splitEntries
 
 validPassport2 :: String -> Bool
-validPassport2 str = all (`elem` fields) allFields && and (zipWith validField fields vals)  where
+validPassport2 str = all (`elem` fields) reqFields && and (zipWith validField fields vals)  where
   entries = splitOn " " str
   splitEntries = map (splitOn ":") entries
   fields = map head splitEntries
